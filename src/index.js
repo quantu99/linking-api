0ca7cf8570512ff90825151488bface44dd8f7c3
Từ Quân <callmequantu@gmail.com>
2024-01-27 02:06:06 +0700

    nothing

--- src/index.js
+++ src/index.js
@@ -15,6 +15,17 @@ app.use(morgan('common'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/v1/link', linkRoute);
+app.get('/instagram-oembed', async (req, res) => {
+    try {
+        const response = await axios.get(
+            `https://api.instagram.com/oembed?url=${req.query.url}&access_token=e13193ff5dadce5500660416f3d19f90`,
+        );
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 app.listen(PORT || 8000, () => {
     console.log(`This app is running on http://localhost:${PORT}`);
 });
